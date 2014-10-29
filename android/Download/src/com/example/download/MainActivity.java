87a4f0556a275a020f35f66adf025c97296b5924
yangsheng <yangsheng1107@gmail.com>
2014-10-29 17:17:25 +0800

    Remove warning message

--- android/Download/src/com/example/download/MainActivity.java
+++ android/Download/src/com/example/download/MainActivity.java
@@ -8,6 +8,7 @@ import java.io.OutputStream;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
@@ -127,7 +128,7 @@ public class MainActivity extends Activity {
 				// Http connect success.
 				HttpResponse httpResponse;
 				httpResponse = httpClient.execute(httpGet);
-				if (httpResponse.getStatusLine().getStatusCode() == 200) {
+				if (httpResponse.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
 					HttpEntity httpEntity = httpResponse.getEntity();
 					inputStream = httpEntity.getContent();
 					lengthOfFile = httpEntity.getContentLength();
